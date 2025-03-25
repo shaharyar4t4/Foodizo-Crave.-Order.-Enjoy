0315d31e9ad76b93d1f3b7ad3bb6ca81f7855174
Shaharyar <shaharyar4t4@gmail.com>
2025-03-25 14:52:40 +0500

    updated 2.0

--- lib/screen/view_dashbored.dart
+++ lib/screen/view_dashbored.dart
@@ -12,6 +12,7 @@ class ViewDashbored extends StatefulWidget {
 }
 
 class _ViewDashboredState extends State<ViewDashbored> {
+
   int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
@@ -72,7 +73,7 @@ class _ViewDashboredState extends State<ViewDashbored> {
                 height: 15,
               ),
               Container(
-                width: 340,
+                width: 350,
                 height: 160,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
@@ -200,112 +201,117 @@ class _ViewDashboredState extends State<ViewDashbored> {
                 ),
               ),
               // Food Items Grid
-              // GridView.count(
-              //   shrinkWrap: true,
-              //   physics: NeverScrollableScrollPhysics(),
-              //   crossAxisCount: 2,
-              //   padding: EdgeInsets.all(16.0),
-              //   childAspectRatio: 0.75,
-              //   children: [
-              //     GestureDetector(
-              //       onTap: () {
-              //         Get.toNamed('/product');
-              //       },
-              //       child: _buildFoodItem(
-              //         image: 'assets/images/image1.png',
-              //         // Replace with your asset path
-              //         title: 'Chicken Burger',
-              //         rating: 3.8,
-              //         description:
-              //             '100 gr chicken burger + tomato + cheese Lettuce',
-              //         price: '\$20.00',
-              //       ),
-              //     ),
-              //     _buildFoodItem(
-              //       image: 'assets/images/image2.png',
-              //       // Replace with your asset path
-              //       title: 'Cheese Burger',
-              //       rating: 4.5,
-              //       description:
-              //           '100 gr meat burger + onion + tomato + Lettuce cheese',
-              //       price: '\$15.00',
-              //     ),
-              //   ],
-              // ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                padding: EdgeInsets.all(16.0),
+                childAspectRatio: 0.66,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed('/product');
+                    },
+                    child: _buildFoodItem(
+                      image: 'assets/images/product1.png',
+                      // Replace with your asset path
+                      title: 'Chicken Burger',
+                      rating: 3.8,
+                      description:
+                          '100 gr chicken burger + tomato + cheese Lettuce',
+                      price: '\$20.',
+                    ),
+                  ),
+                  _buildFoodItem(
+                    image: 'assets/images/product2.png',
+                    // Replace with your asset path
+                    title: 'Cheese Burger',
+                    rating: 4.5,
+                    description:
+                        '100 gr meat burger + onion + tomato + cheese',
+                    price: '\$15.',
+                  ),
+                ],
+              ),
               // Popular Meal Menu
-              // Padding(
-              //   padding: EdgeInsets.all(16.0),
-              //   child: Row(
-              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //     children: [
-              //       Text(
-              //         'Popular Meal Menu',
-              //         style:
-              //             TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              //       ),
-              //       Text(
-              //         'See All',
-              //         style: TextStyle(fontSize: 16, color: sttext),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-              // Container(
-              //   margin: EdgeInsets.symmetric(horizontal: 16.0),
-              //   padding: EdgeInsets.all(16.0),
-              //   decoration: BoxDecoration(
-              //     color: Colors.white,
-              //     borderRadius: BorderRadius.circular(12.0),
-              //     boxShadow: [
-              //       BoxShadow(
-              //         color: Colors.grey.withOpacity(0.2),
-              //         spreadRadius: 2,
-              //         blurRadius: 5,
-              //         offset: Offset(0, 2),
-              //       ),
-              //     ],
-              //   ),
-              //   child: Row(
-              //     children: [
-              //       Image.asset(
-              //         'assets/images/image3.png',
-              //         // Replace with your pizza image asset path
-              //         width: 80,
-              //         height: 80,
-              //         fit: BoxFit.cover,
-              //       ),
-              //       SizedBox(width: 16),
-              //       Expanded(
-              //         child: Column(
-              //           crossAxisAlignment: CrossAxisAlignment.start,
-              //           children: [
-              //             Row(
-              //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //               children: [
-              //                 Text(
-              //                   'Pepper Pizza',
-              //                   style: TextStyle(
-              //                       fontSize: 16, fontWeight: FontWeight.bold),
-              //                 ),
-              //                 Text(
-              //                   '\$15',
-              //                   style: TextStyle(
-              //                       fontSize: 16,
-              //                       fontWeight: FontWeight.bold,
-              //                       color: Colors.pink[300]),
-              //                 ),
-              //               ],
-              //             ),
-              //             Text(
-              //               '5kg box of pizza',
-              //               style: TextStyle(fontSize: 14, color: Colors.grey),
-              //             ),
-              //           ],
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Popular Meal Menu',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'See All',
+                          style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.bold),
+                        ),
+                        Icon(Icons.arrow_forward_ios, color: Colors.grey, size: 17,),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/image3.png',
+                      // Replace with your pizza image asset path
+                      width: 80,
+                      height: 80,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Pepper Pizza',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '\$15',
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: sttext),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            '5kg box of pizza',
+                            style: TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               SizedBox(height: 16),
               // Add some space at the bottom
             ],
@@ -315,40 +321,6 @@ class _ViewDashboredState extends State<ViewDashbored> {
     );
   }
 
-  // Widget _buildCategoryItem(String title, String image, Color color) {
-  //   bool isSelected = false;
-  //   return GestureDetector(
-  //     onTap: () {
-  //       setState(() {
-  //         isSelected = !isSelected;// Toggle selection state
-  //       });
-  //     },
-  //     child: Container(
-  //       margin: EdgeInsets.only(right: 8.0),
-  //       padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-  //       decoration: BoxDecoration(
-  //         color: isSelected ? Colors.pink : Colors.white, // Change color on tap
-  //         borderRadius: BorderRadius.circular(12.0),
-  //         border: Border.all(color: sttext),
-  //       ),
-  //       child: Row(
-  //         mainAxisSize: MainAxisSize.min,
-  //         children: [
-  //           Image.asset(image, height: 24,),
-  //           SizedBox(width: 8),
-  //           Text(
-  //             title,
-  //             style: TextStyle(
-  //               color: isSelected ? Colors.white : Colors.black, // Change text color when selected
-  //               fontWeight: FontWeight.bold,
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
-
   Widget _buildFoodItem({
     required String image,
     required String title,
@@ -357,15 +329,15 @@ class _ViewDashboredState extends State<ViewDashbored> {
     required String price,
   }) {
     return Container(
-      margin: EdgeInsets.all(8.0),
+
+      margin: EdgeInsets.only(right: 10),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(12.0),
+        borderRadius: BorderRadius.circular(10.0),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 5,
+            blurRadius: 6,
             offset: Offset(0, 2),
           ),
         ],
@@ -373,13 +345,29 @@ class _ViewDashboredState extends State<ViewDashbored> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 5, top: 5),
+            child: Row(
+              children: [
+                Icon(Icons.star, color: star_color, size: 20),
+                SizedBox(width: 4),
+                Text(
+                  rating.toString(),
+                  style: TextStyle(fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
           ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(12.0)),
-            child: Image.asset(
-              image,
-              height: 120,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 6),
+              child: Image.asset(
+                image,
+                height: 100,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Padding(
@@ -395,36 +383,47 @@ class _ViewDashboredState extends State<ViewDashbored> {
                       style:
                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
+                  ],
+                ),
+                SizedBox(height: 8,),
+                Text(
+                  description,
+                  style: TextStyle(fontSize: 10, color: Colors.grey, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          price,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: sttext),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: Text(
+                            "00",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: sttext),
+                          ),
+                        ),
+                      ],
+                    ),
                     Container(
                       padding: EdgeInsets.all(4.0),
                       decoration: BoxDecoration(
                         color: sttext,
                         shape: BoxShape.circle,
                       ),
-                      child: Icon(Icons.add, color: Colors.white, size: 16),
+                      child: Icon(Icons.add, color: Colors.white, size: 18),
                     ),
                   ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.star, color: Colors.amber, size: 16),
-                    SizedBox(width: 4),
-                    Text(
-                      rating.toString(),
-                      style: TextStyle(fontSize: 14, color: Colors.grey),
-                    ),
-                  ],
-                ),
-                Text(
-                  description,
-                  style: TextStyle(fontSize: 12, color: Colors.grey),
-                ),
-                Text(
-                  price,
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.pink[300]),
                 ),
               ],
             ),
--- lib/util/cont_color.dart
+++ lib/util/cont_color.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const Color sttext = Color(0xFFd61354);
 const Color sbtn  =  Color(0xFFfa020b);
 const Color sbtn1 =  Color(0xFFFFE1B4);
+const Color star_color = Color(0xFFFF9F06);
 
