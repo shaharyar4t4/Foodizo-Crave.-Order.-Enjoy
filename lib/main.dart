06d630525bb749786d7ee45bf0095fa6c75ea939
Shaharyar <shaharyar4t4@gmail.com>
2025-04-02 15:47:27 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shopstore/components/nav_bar.dart';
+
 import 'package:shopstore/firebase_options.dart';
 import 'package:shopstore/routes/app_routes.dart';
-import 'package:shopstore/screen/veiw_thanks.dart';
-import 'package:shopstore/screen/view_dashbored.dart';
+import 'package:shopstore/screen/view_product.dart';
 import 'package:shopstore/screen/view_splash.dart';
 
 void main() async {
@@ -30,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
       getPages: allPage,
-      home: ViewSplash(),
+      home: ProductPage(),
     );
   }
 }
--- lib/screen/view_product.dart
+++ lib/screen/view_product.dart
@@ -17,14 +17,21 @@ class ProductPage extends StatelessWidget {
                     'assets/images/product.png',
                     fit: BoxFit.cover,
                     width: double.infinity,
-                    height: 500,
+                    height: 400,
                   ),
                   Positioned(
                     top: 20,
                     left: 10,
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back, color: Colors.redAccent),
-                      onPressed: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xAEFFD7D7),
+                        borderRadius: BorderRadius.circular(12),
+                        
+                      ),
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back, color: sttext),
+                        onPressed: () {},
+                      ),
                     ),
                   ),
                 ],
@@ -36,87 +43,87 @@ class ProductPage extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
                     ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 100, right: 100),
-                        child: Center(
-                          child: Container(
-                            padding: EdgeInsets.only(left: 12, right: 12),
-                            height: 10,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                            decoration: BoxDecoration(
-                              color: sttext,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Text(
-                              'Popular',
-                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Spacer(),
-                          Icon(Icons.favorite_border, color: Colors.black),
-                        ],
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Chicken Burger Promo Pack',
-                        style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8),
-                      Row(
-                        children: [
-                          Icon(Icons.star, color: Colors.amber, size: 20),
-                          SizedBox(width: 4),
-                          Text('4.8 Rating', style: TextStyle(color: Colors.grey)),
-                          SizedBox(width: 10),
-                          Text('7000+ Orders', style: TextStyle(color: Colors.grey)),
-                        ],
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'In a medium bowl, add ground chicken, breadcrumbs, mayonnaise, onions, parsley, garlic, paprika, salt and pepper. Use your hands to combine all the ingredients together.',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      SizedBox(height: 20,),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.pinkAccent,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          onPressed: () {
-                            Get.toNamed('order');
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 14),
-                            child: Text(
-                              'Add To Cart',
-                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  // child: Column(
+                  //   crossAxisAlignment: CrossAxisAlignment.start,
+                  //   children: [
+                  //     Padding(
+                  //       padding: const EdgeInsets.only(left: 100, right: 100),
+                  //       child: Center(
+                  //         child: Container(
+                  //           padding: EdgeInsets.only(left: 12, right: 12),
+                  //           height: 10,
+                  //           decoration: BoxDecoration(
+                  //             color: Colors.grey.shade200,
+                  //             borderRadius: BorderRadius.circular(12),
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //     Row(
+                  //       children: [
+                  //         Container(
+                  //           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                  //           decoration: BoxDecoration(
+                  //             color: sttext,
+                  //             borderRadius: BorderRadius.circular(12),
+                  //           ),
+                  //           child: Text(
+                  //             'Popular',
+                  //             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  //           ),
+                  //         ),
+                  //         Spacer(),
+                  //         Icon(Icons.favorite_border, color: Colors.black),
+                  //       ],
+                  //     ),
+                  //     SizedBox(height: 8),
+                  //     Text(
+                  //       'Chicken Burger Promo Pack',
+                  //       style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                  //     ),
+                  //     SizedBox(height: 8),
+                  //     Row(
+                  //       children: [
+                  //         Icon(Icons.star, color: Colors.amber, size: 20),
+                  //         SizedBox(width: 4),
+                  //         Text('4.8 Rating', style: TextStyle(color: Colors.grey)),
+                  //         SizedBox(width: 10),
+                  //         Text('7000+ Orders', style: TextStyle(color: Colors.grey)),
+                  //       ],
+                  //     ),
+                  //     SizedBox(height: 8),
+                  //     Text(
+                  //       'In a medium bowl, add ground chicken, breadcrumbs, mayonnaise, onions, parsley, garlic, paprika, salt and pepper. Use your hands to combine all the ingredients together.',
+                  //       style: TextStyle(color: Colors.black),
+                  //     ),
+                  //     SizedBox(height: 20,),
+                  //     SizedBox(
+                  //       width: double.infinity,
+                  //       child: ElevatedButton(
+                  //         style: ElevatedButton.styleFrom(
+                  //           backgroundColor: Colors.pinkAccent,
+                  //           shape: RoundedRectangleBorder(
+                  //             borderRadius: BorderRadius.circular(12),
+                  //           ),
+                  //         ),
+                  //         onPressed: () {
+                  //           Get.toNamed('order');
+                  //         },
+                  //         child: Padding(
+                  //           padding: EdgeInsets.symmetric(vertical: 14),
+                  //           child: Text(
+                  //             'Add To Cart',
+                  //             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
                 ),
               ),
             ],
