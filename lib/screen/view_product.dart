0dff2200c8c1ec4ae4bd5e7917d7a7e0648e346d
Shaharyar <shaharyar4t4@gmail.com>
2025-04-02 21:35:57 +0500

    updated2.8

--- lib/screen/view_product.dart
+++ lib/screen/view_product.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopstore/util/cont_color.dart';
@@ -7,123 +8,166 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Image.asset(
-                    'assets/images/product.png',
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: 380,
-                  ),
-                  Positioned(
-                    top: 20,
-                    left: 10,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xAEFFD7D7),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: IconButton(
-                        icon: Icon(Icons.arrow_back, color: sttext),
-                        onPressed: () {
-                          Get.back();
-                        },
-                      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/product.png'),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 100, right: 100),
-                    child: Center(
-                      child: Container(
-                        padding: EdgeInsets.only(left: 12, right: 12),
-                        height: 10,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(12),
+                ),
+                Positioned(
+                  top: 20,
+                  left: 20,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xAEFFD7D7),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back, color: sttext),
+                      onPressed: () {
+                        Get.back();
+                      },
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.45,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height *
+                        0.65, // Reduced height
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            right: 130,
+                            left: 130,
+                            top: 10,
+                          ),
+                          child: Container(
+                            height:
+                                6, // Added height to the inner container for visibility
+                            decoration: BoxDecoration(
+                              color: bar_color, // Ensure bar_color is defined
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                  Row(
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
                     children: [
                       Container(
                         padding:
                             EdgeInsets.symmetric(horizontal: 15, vertical: 8),
                         decoration: BoxDecoration(
-                          color: sttext,
+                          color: bar_color,
                           borderRadius: BorderRadius.circular(12),
                         ),
                         child: Text(
                           'Popular',
                           style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                              color: sttext, fontWeight: FontWeight.bold),
                         ),
                       ),
                       Spacer(),
-                      Icon(Icons.favorite_border, color: Colors.black),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Chicken Burger Promo Pack',
-                    style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Icon(Icons.star, color: Colors.amber, size: 20),
-                      SizedBox(width: 4),
-                      Text('4.8 Rating', style: TextStyle(color: Colors.grey)),
-                      SizedBox(width: 10),
-                      Text('7000+ Orders',
-                          style: TextStyle(color: Colors.grey)),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'In a medium bowl, add ground chicken, breadcrumbs, mayonnaise, onions, parsley, garlic, paprika, salt and pepper. Use your hands to combine all the ingredients together.',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.pinkAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
+                      Container(
+                        padding: EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                            color: bar_color,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Icon(
+                          CupertinoIcons.location_solid,
+                          color: sttext,
                         ),
                       ),
-                      onPressed: () {
-                        Get.toNamed('order');
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 14),
-                        child: Text(
-                          'Add To Cart',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
+                      SizedBox(
+                        width: 13,
                       ),
-                    ),
+                      Container(
+                        padding: EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                            color: cir_color,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Icon(CupertinoIcons.heart, color: Colors.black),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Chicken Burger Promo Pack',
+                  style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                // Row(
+                //   children: [
+                //     Icon(Icons.star, color: Colors.amber, size: 20),
+                //     SizedBox(width: 4),
+                //     Text('4.8 Rating', style: TextStyle(color: Colors.grey)),
+                //     SizedBox(width: 10),
+                //     Text('7000+ Orders', style: TextStyle(color: Colors.grey)),
+                //   ],
+                // ),
+                SizedBox(height: 8),
+                // Text(
+                //   'In a medium bowl, add ground chicken, breadcrumbs, mayonnaise, onions, parsley, garlic, paprika, salt and pepper. Use your hands to combine all the ingredients together.',
+                //   style: TextStyle(color: Colors.black),
+                // ),
+                SizedBox(
+                  height: 20,
+                ),
+                // SizedBox(
+                //   width: double.infinity,
+                //   child: ElevatedButton(
+                //     style: ElevatedButton.styleFrom(
+                //       backgroundColor: Colors.pinkAccent,
+                //       shape: RoundedRectangleBorder(
+                //         borderRadius: BorderRadius.circular(12),
+                //       ),
+                //     ),
+                //     onPressed: () {
+                //       Get.toNamed('order');
+                //     },
+                //     child: Padding(
+                //       padding: EdgeInsets.symmetric(vertical: 14),
+                //       child: Text(
+                //         'Add To Cart',
+                //         style: TextStyle(
+                //             fontSize: 18,
+                //             fontWeight: FontWeight.bold,
+                //             color: Colors.white),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/util/cont_color.dart
+++ lib/util/cont_color.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color sttext = Color(0xFFd61354);
-const Color sbtn  =  Color(0xFFfa020b);
-const Color sbtn1 =  Color(0xFFFFE1B4);
+const Color sbtn = Color(0xFFfa020b);
+const Color sbtn1 = Color(0xFFFFE1B4);
 const Color star_color = Color(0xFFFF9F06);
-
+const Color bar_color = Color.fromARGB(80, 255, 203, 203);
+const Color cir_color = Color.fromARGB(112, 220, 219, 219);
